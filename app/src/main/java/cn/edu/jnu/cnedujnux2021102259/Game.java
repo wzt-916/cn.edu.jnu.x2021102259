f603e1ca9eac80651ad04249b025be14e5e3d61b
wzt-916 <2230549589@qq.com>
2024-05-07 07:40:48 +0800

    测试二通过

--- app/src/main/java/cn/edu/jnu/cnedujnux2021102259/Game.java
+++ app/src/main/java/cn/edu/jnu/cnedujnux2021102259/Game.java
@@ -1,10 +1,13 @@
 package cn.edu.jnu.cnedujnux2021102259;
-
 public class Game {
-    public void roll(int i) {
+    private int score = 0;
+
+    public void roll(int pins) {
+        score += pins;
     }
 
     public int score() {
-        return 0;
+        return score;
     }
 }
+
--- app/src/test/java/cn/edu/jnu/cnedujnux2021102259/BowlingGameTest.java
+++ app/src/test/java/cn/edu/jnu/cnedujnux2021102259/BowlingGameTest.java
@@ -4,36 +4,33 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import junit.framework.TestCase;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class BowlingGameTest {
-    @Test
+public class BowlingGameTest extends TestCase {
+    private Game g;
+
+    protected void setUp() throws Exception {
+        g = new Game();
+    }
+
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++)
+            g.roll(pins);
+    }
+
     public void testGutterGame() throws Exception {
-        Game g = new Game();
-        for (int i=0; i<20; i++)
-            g.roll(0);
+        rollMany(20, 0);
         assertEquals(0, g.score());
     }
+
     public void testAllOnes() throws Exception {
-        Game g = new Game();
-        for (int i = 0; i < 20; i++)
-            g.roll(1);
+        rollMany(20,1);
         assertEquals(20, g.score());
     }
-
 }
 
-/*import junit.framework.TestCase;
-
-public class BowlingGameTest extends TestCase {
-    public void testGutterGame() throws Exception {
-        Game g = new Game();
-        for (int i=0; i<20; i++)
-            g.roll(0);
-        assertEquals(0, g.score());
-    }
-}
-*/
