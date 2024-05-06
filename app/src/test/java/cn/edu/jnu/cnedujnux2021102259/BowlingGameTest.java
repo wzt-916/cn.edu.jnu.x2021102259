59a3a41d0b804ace08bff92bf4fd6f09bd115a96
wzt-916 <2230549589@qq.com>
2024-05-07 07:37:02 +0800

    测试二

--- app/src/test/java/cn/edu/jnu/cnedujnux2021102259/BowlingGameTest.java
+++ app/src/test/java/cn/edu/jnu/cnedujnux2021102259/BowlingGameTest.java
@@ -17,6 +17,13 @@ public class BowlingGameTest {
             g.roll(0);
         assertEquals(0, g.score());
     }
+    public void testAllOnes() throws Exception {
+        Game g = new Game();
+        for (int i = 0; i < 20; i++)
+            g.roll(1);
+        assertEquals(20, g.score());
+    }
+
 }
 
 /*import junit.framework.TestCase;
